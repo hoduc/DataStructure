4c03fec687ce16f800efc4a9ca96584c0ba2d745
Duc Ho <duc_ho90@yahoo.com>
2012-10-08 17:04:51 -0400

    recode

--- GameOfLife/gol_demo.cpp
+++ GameOfLife/gol_demo.cpp
@@ -1,41 +1,126 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+#include <cstdlib>
+#include <windows.h>
 
-enum STATE{DEAD, LIVE};
+enum STATE{DEAD, LIVE, ZOMBIE};
 
 struct cell{
 	STATE NOW;
 	STATE FUTURE;
-	cell():NOW(DEAD),FUTURE(DEAD){};
+	cell():NOW(DEAD),FUTURE(ZOMBIE){};
 };
 
 cell **map;
-int row = 0;
-int col = 0;
+int row = 40;
+int col = 40;
 
 void setup(){
 	std::fstream im;		//input map
 	//read in row and column
 	im.open("map.txt", std::ios::in);
 	
-	im >> row >> col;
 	map = new cell*[row];
 	for (int i = 0; i < row; ++i){
 		map[i] = new cell[col];
 	}
-
-	im.seekg(col, std::ios::beg);
+	
+	std::string line = "";
+	im.seekg(0, std::ios::beg);
 	for (int i = 0; i < row; ++i){
-		for (int j = 0; j < col; ++j){
-			char c = im.get();
-			if (c == '*')
-				map[i][j].NOW = LIVE;
-			else if (c == ' ')
+		getline(im,line);
+		for (int j = 0; j < col ;++j){
+			if (j > line.length()){
 				map[i][j].NOW = DEAD;
+				continue;
+			}
+			if (line[j] == 'L'){
+				map[i][j].NOW = LIVE;
+			}
+			else if (((line[j] == 'D') || (line[j] == ' '))){
+				map[i][j].NOW = DEAD;
+			}
 		}
-		im.get();
 	}
 	
+}
+
+void populate(){
+	int neighbor_counter;
+	for (int i = 0; i < row; ++i){
+		for (int j = 0; j < col; ++j){
+			neighbor_counter = 0;
+			//top-left
+			if (((i-1) >= 0) && ((j-1) >= 0)){
+				if (map[i-1][j-1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//top-mid
+			if (((i-1) >= 0) && (j >= 0)){
+				if (map[i-1][j].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//top-right
+			if (((i-1) >= 0) && ((j+1) < col)){
+				if (map[i-1][j+1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//left
+			if ((i >= 0) && ((j-1) >= 0)){
+				if (map[i][j-1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//right
+			if ((i >= 0) && (j+1) < col){
+				if (map[i][j+1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//low-left
+			if (((i+1) < row) && ((j-1) >= 0)){
+				if (map[i+1][j-1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//low-mid
+			if (((i+1) < row) && (j >= 0)){
+				if (map[i+1][j].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//low-right
+			if (((i+1) < row) && ((j+1) < col)){
+				if (map[i+1][j+1].NOW == LIVE)
+					neighbor_counter++;
+			}
+			//change the now -cell
+			
+			//is current cell dead or alive
+			if (map[i][j].NOW == LIVE){
+				//DEAD b/c of under-population
+				if (neighbor_counter < 2)
+					map[i][j].FUTURE = DEAD;
+				//STILL LIVES
+				else if (neighbor_counter >=2 && neighbor_counter <= 3)
+					map[i][j].FUTURE = LIVE;
+				//DEAD b/c of over-crowdin
+				else if (neighbor_counter > 3)
+					map[i][j].FUTURE = DEAD;
+			}
+			else{
+				//DEAD->LIVE b/c of reproduction
+				if (neighbor_counter == 3)
+					map[i][j].FUTURE = LIVE;
+			}
+
+		}
+	}
+
+	//populate
+	for (int i = 0; i < row; ++i){
+		for (int j = 0; j < col; ++j){
+			map[i][j].NOW = map[i][j].FUTURE;
+			map[i][j].FUTURE = ZOMBIE;
+		}
+	}
 }
 
 void draw(){
@@ -51,14 +136,19 @@ void draw(){
 
 		std::cout << std::endl;
 	}
+	//std::cout << "---------------------" << std::endl;
 }
 
 int main(){
 	setup();
 	draw();
-	//while(1){
-		//update();
-		//draw();
-	//}
+	
+	while(1){
+		Sleep(1000);
+		system("cls");
+		populate();
+		draw();
+		
+	}
 	return 0;
 }
